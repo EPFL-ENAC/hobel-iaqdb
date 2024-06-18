493bd3d5d98d076ac6c6d58d0c67be9b38443109
ymarcon <yannick.marcon@epfl.ch>
2024-06-18 08:41:44 +0200

    fix: image url

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page>
-    <app-header url="/interior-1.jpg" />
+    <app-header url="interior-1.jpg" />
     <div class="row">
       <div class="col"></div>
       <div class="col-8">
