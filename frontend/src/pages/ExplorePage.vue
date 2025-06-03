a4e198a41233936d81c600cc67cf61c499a79abf
ymarcon <yannick.marcon@epfl.ch>
2025-06-03 13:36:56 +0200

    feat: added info toolbar in explore page

--- frontend/src/pages/ExplorePage.vue
+++ frontend/src/pages/ExplorePage.vue
@@ -1,16 +1,30 @@
 <template>
-  <q-page class="q-pa-md">
-    <div class="row q-col-gutter-md">
-      <div class="col-12 col-sm-12 col-md-6">
-        <DummyCandlestickChart :height="200"/>
-      </div>
-      <div class="col-12 col-sm-12 col-md-6">
-        <DummyBarChart :height="200"/>
-      </div>
+  <q-page>
+    <q-toolbar class="bg-accent text-white">
+      
+    <div>
+      This section is under construction. For suggestions of useful plots and features, please file a 
+      <a
+        href="https://github.com/EPFL-ENAC/hobel-iaqdb/issues"
+        target="_blank"
+        class="text-white text-bold epfl"
+        >GitHub issue <q-icon name="arrow_outward" /></a
+      >.
     </div>
-    <div class="row q-col-gutter-md">
-      <div class="col-12">
-        <DummyLineChart :height="200"/>
+  </q-toolbar>
+    <div class="q-pa-md">
+      <div class="row q-col-gutter-md">
+        <div class="col-12 col-sm-12 col-md-6">
+          <DummyCandlestickChart :height="200"/>
+        </div>
+        <div class="col-12 col-sm-12 col-md-6">
+          <DummyBarChart :height="200"/>
+        </div>
+      </div>
+      <div class="row q-col-gutter-md">
+        <div class="col-12">
+          <DummyLineChart :height="200"/>
+        </div>
       </div>
     </div>
   </q-page>
@@ -20,5 +34,4 @@
 import DummyCandlestickChart from 'src/components/plots/DummyCandlestickChart.vue';
 import DummyBarChart from 'src/components/plots/DummyBarChart.vue';
 import DummyLineChart from 'src/components/plots/DummyLineChart.vue';
-
 </script>
